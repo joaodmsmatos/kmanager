f9bfb9cb68d01738dfa49f4189e99d0d4f18eac6
joaodmsmatos@gmail.com <joaodmsmatos@gmail.com>
2018-05-17 23:13:41 +0200

    update class headers

--- kmanager/src/main/java/com/matossoft/kmanager/ui/ViewController.java
+++ kmanager/src/main/java/com/matossoft/kmanager/ui/ViewController.java
@@ -1,18 +1,28 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright (C) 2018 https://github.com/joaodmsmatos/
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package com.matossoft.kmanager.ui;
 
 import com.matossoft.kmanager.utils.UIHelper;
 import java.util.Observable;
 import java.util.Observer;
-import javax.swing.SwingUtilities;
 
 /**
  *
- * @author joaod
+ * @author https://github.com/joaodmsmatos/
  */
 public class ViewController extends javax.swing.JFrame implements Observer{
 
@@ -64,17 +74,6 @@ public class ViewController extends javax.swing.JFrame implements Observer{
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-
-        /* Create and display the form */ 
-        SwingUtilities.invokeLater(() -> {
-            new ViewController().setVisible(true);
-        });
-    }
 
     @Override
     public void update(Observable o, Object arg) {
